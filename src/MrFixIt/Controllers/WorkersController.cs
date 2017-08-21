eeb470981374ce695f4abb84fa6c62107a28b0ed
Pete Lazuran <pdlazuran@gmail.com>
2017-08-21 04:16:42 -0700

    haveall required functionality

--- src/MrFixIt/Controllers/WorkersController.cs
+++ src/MrFixIt/Controllers/WorkersController.cs
@@ -6,8 +6,6 @@ using Microsoft.AspNetCore.Mvc;
 using MrFixIt.Models;
 using Microsoft.EntityFrameworkCore;
 
-// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
-
 namespace MrFixIt.Controllers
 {
     public class WorkersController : Controller
@@ -38,6 +36,42 @@ namespace MrFixIt.Controllers
         {
             worker.UserName = User.Identity.Name;
             db.Workers.Add(worker); 
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult JobPending(int id)
+        {
+            var thisItem = db.Jobs.FirstOrDefault(items => items.JobId == id);
+            return View(thisItem);
+        }
+
+        [HttpPost]
+        public IActionResult JobPending(Job job)
+        {
+            var thisWorker = db.Workers.FirstOrDefault(i => i.UserName == User.Identity.Name);
+            thisWorker.Avaliable = false;
+            job.Pending = true;
+            db.Entry(job).State = EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult JobComplete(int id)
+        {
+            var thisItem = db.Jobs.FirstOrDefault(items => items.JobId == id);
+            return View(thisItem);
+        }
+
+        [HttpPost]
+        public IActionResult JobComplete(Job job)
+        {
+            var thisWorker = db.Workers.FirstOrDefault(i => i.UserName == User.Identity.Name);
+            thisWorker.Avaliable = true;
+            job.Pending = false;
+            job.Completed = true;
+            job.Worker = db.Workers.FirstOrDefault(i => i.UserName == User.Identity.Name);
+            db.Entry(job).State = EntityState.Modified;
             db.SaveChanges();
             return RedirectToAction("Index");
         }
